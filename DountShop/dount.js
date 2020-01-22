845ff74468448aef4054402f8472622d9e9e4ce6
RenDev <menertson@gmail.com>
2020-01-22 01:03:35 -0800

    Added a new porject into index.html

---
+++ DountShop/dount.js
@@ -0,0 +1,16 @@
+function left() {
+  var x = document.getElementById("LeftAr");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myFunction2() {
+  var y = document.getElementById("myDIV2");
+  if (y.style.display === "none") {
+    y.style.display = "block";
+  } else {
+    y.style.display = "none";
+  }
+}
